d389480c2d028122552d1486ef548095a4fe98d2
KotyhovaAnna <anna.kotihova@mail.ru>
2020-05-30 00:05:27 +0300

    Add

--- NUnitTestProject5/NUnitTestProject5/UnitTest1.cs
+++ NUnitTestProject5/NUnitTestProject5/UnitTest1.cs
@@ -1,25 +1,41 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
 using System.Text;
+
+
 namespace NUnitTestProjectSeleniumWebDriverAdvanced
 {
+
     class AddProductPage
     {
+        private readonly IWebElement ProductName;
+        private readonly IWebElement clickProduct;
         private IWebDriver driver;
+        private readonly string element;
+        private IWebElement UnitPrice;
+        private IWebElement CategoryId;
+        private IWebElement SupplierId;
+        private IWebElement QuantityPerUnit;
+        private IWebElement UnitsOnOrder;
+        private IWebElement ReorderLevel;
+        private IWebElement clickCreateNew;
+        private IWebElement Product;
+        private IWebElement ClickAll_Products;
+        private IWebElement ClickProducts;
+        private IWebElement ClickHome_page;
+
         public AddProductPage(IWebDriver driver)
         {
             this.driver = driver;
         }
+
         public IWebElement Home_page { get; private set; }
         public IWebElement CreateNew { get; private set; }
-        public string CategoryId { get; private set; }
-        public string SupplierId { get; private set; }
-        public string UnitPrice { get; private set; }
-        public string QuantityPerUnit { get; private set; }
-        public string ReorderLevel { get; private set; }
-        public string UnitsOnOrder { get; private set; }
+
+
+
         private IWebElement searchHome_page => driver.FindElement(By.XPath("//div[h2='Home page']//preceding::a[@href ='/Product']"));
         private IWebElement searchCreateNew => driver.FindElement(By.XPath("//a[@class = 'btn btn-default']"));
         private IWebElement searchProductName => driver.FindElement(By.XPath("//a[@class = 'btn btn-default']"));
@@ -31,28 +47,38 @@ namespace NUnitTestProjectSeleniumWebDriverAdvanced
         private IWebElement searchUnitsOnOrder => driver.FindElement(By.XPath("//input[@id = 'UnitsOnOrder']"));
         private IWebElement searchReorderLevel => driver.FindElement(By.XPath("//input[@id = 'ReorderLevel']"));
         private IWebElement searchsubmit => driver.FindElement(By.XPath("//input[@type = 'submit']"));
-        //public void AddNewProducts(string productProductName)
-        //{
-        //    searchHome_page.Click();
-        //    searchCreateNew.Click();
-        //    searchProductName.SendKeys(productProductName);
-        //}
-        public void AddNewProduct()
+
+        public IWebElement ClickProduct => clickProduct;
+
+        public string Element => element;
+
+        public string Element1 => element;
+
+        //public void AddNewProducts(string productProductName)
+        //{
+        //    searchHome_page.Click();
+        //    searchCreateNew.Click();
+        //    searchProductName.SendKeys(productProductName);
+
+        //}
+
+        public void AddNewProduct()
         {
-            new Actions(driver).Click(Products).Build().Perform();
-            new Actions(driver).Click(CreateNew).Build().Perform();
-            new Actions(driver).SendKeys("морс").Build().Perform();
-            new Actions(driver).SendKeys(CategoryId).Build().Perform();
-            new Actions(driver).SendKeys(SupplierId).Build().Perform();
-            new Actions(driver).SendKeys(UnitPrice).Build().Perform();
-            new Actions(driver).SendKeys(QuantityPerUnit).Build().Perform();
-            new Actions(driver).SendKeys(UnitsOnOrder).Build().Perform();
-            new Actions(driver).SendKeys(ReorderLevel).Build().Perform();
+            new Actions(driver).Click(ClickProducts).Build().Perform();
+            new Actions(driver).Click(clickCreateNew).Build().Perform();
+            new Actions(driver).SendKeys(ProductName, "����").Build().Perform();
+            new Actions(driver).Click(CategoryId).SendKeys("7").Build().Perform();
+            new Actions(driver).Click(SupplierId).SendKeys("10").Build().Perform();
+            new Actions(driver).Click(UnitPrice).SendKeys("30,0000").Build().Perform();
+            new Actions(driver).Click(QuantityPerUnit).SendKeys("95").Build().Perform();
+            new Actions(driver).Click(UnitsOnOrder).SendKeys("74").Build().Perform();
+            new Actions(driver).Click(ReorderLevel).SendKeys("8").Build().Perform();
             new Actions(driver).SendKeys(Keys.Enter).Build().Perform();
         }
         internal void AddNewProducts()
         {
             throw new NotImplementedException();
         }
+
     }
 }
